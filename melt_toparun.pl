7be1d70365c0228a51dc64a14f2037d9730f2052
Bushmarinov <ib@xrlab.ru>
2015-05-29 00:34:56 +0300

    improve melt_toparun.pl
    custom filename output; mre legible error processing

--- melt_toparun.pl
+++ melt_toparun.pl
@@ -3,14 +3,23 @@
 use Modern::Perl '2013';
 use autodie;
 use Text::CSV;
+use Getopt::Long;
 
-die <<HELP unless @ARGV;
-Usage: melt_toparun.pl <dirnames>
+my ($outfile, $help);
+
+GetOptions('outfile=s' => \$outfile,
+           'help|h|?' => \$help,
+           
+           );
+die <<HELP if $help or !@ARGV;
+Usage: melt_toparun.pl [-o outfile] <dirnames>
 
 Prits out toparunning result from given directories in molten style.
 Accepts wildcards in dirnames even when shell does not (i.e. on Win32).
 HELP
 
+$outfile ||= 'molten_toparun.txt';
+$outfile .= '.txt' unless $outfile =~ /\./;
 @ARGV = grep {-d $_} map { glob $_ } @ARGV;
 
 my @molten = [qw/Name Bond Ideal Lower Upper/];
@@ -19,6 +28,8 @@ DIR:
 foreach my $dir (@ARGV) {
     my $tableh;
     eval {
+        -e "$dir/toparunning_result.txt" or die "toparunning_result.txt does not exist!\n";
+        -s "$dir/toparunning_result.txt" or die "toparunning_result.txt is empty!\n";
         open $tableh, "$dir/toparunning_result.txt";
         my $csv = Text::CSV->new({eol => undef, sep_char => "\t", allow_whitespace => 1});
         $csv->column_names(map {lc $_} @{$csv->getline($tableh)});  #use lc'd headers and do not care about order
@@ -28,13 +39,13 @@ foreach my $dir (@ARGV) {
         }  
     };
     if ($@) {
-        say "Problems with opening toparunning_result.txt\nin `$dir':\n$@Moving on...";
+        say "Problems with opening toparunning_result.txt in `$dir':\n$@Moving on...\n";
         next DIR;
     }    
 }
 
-open my $outh, '>', 'molten_toparun.txt';
+open my $outh, '>', $outfile;
 
 say $outh join "\t", @$_ foreach @molten;
 
-say "Output written to molten_toparun.txt";
+say "Output written to $outfile";
