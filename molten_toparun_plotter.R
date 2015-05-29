dd48714d76a9acd6da80f51ff8a178cb414cbd8b
Bushmarinov <ib@xrlab.ru>
2015-05-29 05:38:50 +0300

    various modifications to molten_plotter
    recommended calling now : -l -L -W 8.85 -H 10 sugar_instr.txt -R 4

--- molten_toparun_plotter.R
+++ molten_toparun_plotter.R
@@ -7,11 +7,17 @@ suppressMessages(library(optparse))
 ## Create options
 option_list <- list(
     make_option(c("-W", "--width"),type="double", default=17,
-                help="Width of the output file, [default %default]"),
+                help="Width of the output image, cm [default %default]"),
     make_option(c("-H", "--height"), type="double",default=10,
-                help="Height of the output file, [default %default]"),
+                help="Height of the output image, cm [default %default]"),
+	make_option(c("-R", "--rows"), type="integer",default=1,
+                help="Number of rows in legend [default %default]"),
     make_option(c("-f", "--fill"), action="store_true", default=FALSE,
-                help="Fill density plot, [default %default]")
+                help="Fill density plot, [default %default]"),
+	make_option(c("-l", "--linetype"), action="store_true", default=FALSE,
+                help="Vary linetype as well as plot color, [default %default]"),
+	make_option(c("-L", "--line"), action="store_true", default=FALSE,
+                help="Plot lines, not areas [default %default]")
     )
 
 parser    <- OptionParser(usage = "molten_toparun_plotter.r [options] data.txt\n were data.txt is the output of melt_toparun.pl", option_list=option_list)
@@ -28,15 +34,35 @@ if (larg != 1)
 
 ## Read data
 toparun <- read.table(arguments$args, sep="\t", header=T)
+if (!is.factor(toparun$Name)) {
+  toparun$Name <- factor(toparun$Name, levels = sort(unique(toparun$Name)), ordered=TRUE)   # convert integer dataset names to ordered factors
+}
+
+view <- geom_density(alpha=0.2)
+
+if (opt$line) {
+	view <- geom_line(stat="density")
+} 
 
 ## Generate plot object
-p <- ggplot(data=toparun, aes(x=(Upper-Lower)/2, color = Name, group=Name)) +
-    geom_density(alpha=.2) + theme_bw() + xlab("HUW")
+p <- ggplot(data=toparun, aes(x=(Upper-Lower)/2, colour=Name, group=Name)) + view +
+    theme_bw() + xlab("HUW")+ theme(legend.position = "bottom") + labs(colour=NULL) 
 
 ## Modify if fill=TRUE
 if (opt$fill) {p <- p + aes(fill = Name)}
 
+if (opt$linetype) {p <- p + aes(linetype = Name) + labs(linetype=NULL)}
+
+
+
+if (opt$rows > 1) {
+	g <- guide_legend(nrow = opt$rows, byrow = TRUE)
+	p <- p + guides(col = g, linetype = g)
+}
+
 ## Plot
 name = sub("[.].{2,3}","",arguments$args)
-ggsave(p, file= paste(name, "_plot.pdf", sep = ""), width=opt$width, height=opt$height, units="cm")
-ggsave(p, file= paste(name, "_plot.png", sep = ""), width=opt$width, height=opt$height, units="cm")
+# ggsave(p, file= paste(name, "_plot.pdf", sep = ""), width=opt$width, height=opt$height, units="cm")
+ggsave(p, file= paste(name, "_plot.eps", sep = ""), width=opt$width, height=opt$height, units="cm")
+ggsave(p +scale_colour_brewer(palette="Dark2") , file= paste(name, "_plot_brewer.eps", sep = ""), width=opt$width, height=opt$height, units="cm")
+# ggsave(p, file= paste(name, "_plot.png", sep = ""), width=opt$width, height=opt$height, units="cm")
